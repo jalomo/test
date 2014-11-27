e7ed4c819b940553f2513ece31a8bbef6fb70648
jalomo larios <jalomo@hotmail.es>
2014-11-27 02:26:04 -0600

    primera prueba

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+
+
+<style>
+
+
+</style>
+
+
+<link rel="stylesheet" href="css/main.css" type="text/css" media="screen, mobile" title="main" charset="utf-8">
+</head>
+
+<body class="fondo">
+<form action="menu.php">
+<div align="center">
+			<div class="font-nexa tamano28" align="center">Bienvenido</div>
+               
+    		<div class="login- font-nexa " align="center">
+            	<input type="text" class="texto- font-nexa" placeholder="Usuario" />
+                <input  type="password" class="texto- font-nexa" placeholder="Contraseña" />
+                
+                <button class=" boton-">Entrar</button>
+            
+            </div>
+</div>            
+</form>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+
+<style>
+.menu-{ width:200px; height:900px; background:#2c3e50; border-radius:5px; }
+
+.contenido-menu{ width:810px;} 
+
+
+.menus-{ height:40px; color:#ecf0f1; border-radius:10px;}
+
+.menus-:hover{ background:#34495e;}
+
+.menus-texto{ text-decoration:none; cursor:pointer; margin-left:20px; top:10px; position:relative; color:#ecf0f1; width:198px; font-size:20px; }
+.span-top{margin-top:10px;}
+
+.margen-top{ margin-top:10px;}
+
+.fomulario-{ width:600px; height:250px;  margin-left:210px; color:#ecf0f1; border-radius:5px; border-radius:5px;  border:2px solid #16a085;}
+
+.entrada{ width:280px;border-radius:5px; height:30px;border:2px solid #16a085; color:#16a085; font-size:18px; background-color: rgba(0,0,0,0.1); }
+
+
+body{ background:url(img/fondo.png) repeat;}
+
+.lista-m{ width:200px; height:50px; color:#2c3e50; font-size:18px; }
+
+.fomulario-lista{ width:600px; height:630px;  margin-left:210px; color:#ecf0f1; border-radius:5px; border-radius:5px; margin-top:10px; border:2px solid #16a085;}
+</style>
+<link rel="stylesheet" href="css/main.css" type="text/css" media="screen, mobile" title="main" charset="utf-8">
+</head>
+
+<c>
+
+<div align="center">
+
+<div class="float- contenido-menu" align="left">
+
+
+<div class="menu- float-">
+		<div align="center" style="margin-top:5px;">
+		<img src="img/icono.png" width="170" />
+		</div>
+		<div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Usuarios</a></span></div>
+        
+        <div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Clientes</a></span></div>
+        
+        <div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Productos</a></span></div>
+        
+        <div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Proyectos</a></span></div>
+        
+        <div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Facturas</a></span></div>
+        
+        <div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Reportes</a></span></div>
+        
+        <div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Citas</a></span></div>
+        
+        <div class="margen-top"></div>
+        
+		<div class="menus-"><span class="span-top font-nexa"> <a href="#" class="menus-texto"> Salir</a></span></div>
+
+
+</div>
+
+
+
+    <div class="fomulario- font-nexa">
+        <div align="center" style="font-size:25px; margin-top:10px;margin-bottom:20px; border-radius:5px 5px 0px 0px; color:#16a085; " > Usuarios</div>
+        <div align="center">
+        <table width="200" border="0">
+          <tr>
+            <td> <input  type="text" class="entrada" placeholder="Nombre"/></td>
+            <td> <input  type="text" class="entrada" placeholder="Email"/></td>
+          </tr>
+          <tr>
+            <td> <input  type="text" class="entrada" placeholder="Teléfono"/></td>
+            <td> <input  type="text" class="entrada" placeholder="Ciudad"/></td>
+          </tr>
+          <tr>
+            <td> <input  type="text" class="entrada" placeholder="Colonia"/></td>
+            <td> <input  type="text" class="entrada" placeholder="Estado"/></td>
+          </tr>
+        </table>
+        <div align="right" style="margin-right:10px; margin-top:-30px;"><button class=" boton-">Guardar</button></div>
+    </div>
+    
+    </div>
+    
+    
+    
+    <div class="fomulario-lista font-nexa">
+        <div align="center" style="font-size:25px; margin-top:10px;margin-bottom:20px; border-radius:5px 5px 0px 0px; color:#2c3e50; " >Lista usuarios</div>
+        <div align="center">
+        	
+            
+            <div> 
+            	<div class="float- lista-m"  >Nombre</div>
+                <div class="float- lista-m">Email</div>
+                <div class="float- lista-m">Opciones</div>
+                
+                <br/>
+                <div style="background:#16a085; height:2px;"></div>
+                <div class="limpiar"></div>
+            </div>
+
+        
+    </div>
+    
+    </div>
+
+
+
+</div>
+
+
+</div>
+
+</body>
+</html>
